395ed308c11fada8bd8f3fa90b519064a9d34409
Zohar Azugi <zohar927@gmail.com>
2026-01-18 17:22:59 +0200

    add data handler file

---
+++ src/data_handler.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import logging
+
+# Load the data from CSV file
+def load_csv(path):
+    logging.info(f"Loading dataset from {path}")
+    data = pd.read_csv(path)
+    logging.info(f"Dataset loaded: {data.shape[0]} rows, {data.shape[1]} columns")
+    return data
+
+
+# Clean the dataset from duplicates and empty cells
+def clean_data(data):
+    clean_data = data.drop_duplicates().dropna()
+    logging.info(f"Data cleaned. New size: {data.shape[0]} rows, {data.shape[1]} columns")
+    return clean_data
+
+# Get lower and upper bounds for IQR method
+def get_iqr_bounds(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    return lower_bound, upper_bound
+
+# Remove outliers for specified columns using IQR method
+def remove_iqr_outliers(data, columns):
+    initial_rows_count = data.shape[0]
+    clean_data = data
+
+    for col in columns:
+        lower_bound, upper_bound = get_iqr_bounds(data, col)
+        clean_data = clean_data[(clean_data[col] >= lower_bound) & 
+                                (clean_data[col] <= upper_bound)]
+    
+    logging.info(f"Outlier removal done. Removed {initial_rows_count - clean_data.shape[0]} rows.")
+    
+    return clean_data
