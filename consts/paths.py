e376adfae6f2edbdbce3ca8e122a22d1f9c724ea
Noya Hezkia <noya.hezkia@gmail.com>
2026-01-23 22:55:59 +0200

    Add logs file path to paths file

--- consts/paths.py
+++ consts/paths.py
@@ -1,1 +1,2 @@
+LOG_FILE_PATH = "logs/analysis.log"
 DATASET_PATH = "data/teen_phone_addiction_dataset.csv"
--- src/logger_config.py
+++ src/logger_config.py
@@ -1,11 +1,10 @@
 import logging
+from consts.paths import LOG_FILE_PATH
 from pathlib import Path
-
-# Set the path for the log file
-log_file_path = Path("logs") / "analysis.log"
 
 # Configure logger and logs formats
 def setup_logger():
+    log_file_path = Path(LOG_FILE_PATH)
     
     # Create logs folder if does not exist
     log_file_path.parent.mkdir(parents=True, exist_ok=True)
